3eddd0a18dac2f0c14feb8ac269b28ca04aa7eee
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2023-10-29 17:26:27 +0900

    update(_app): add chakra provider & layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Layout from "@/components/Layout";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import "@fontsource/bagel-fat-one/400.css";
+import { theme } from "@/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
+  );
 }
