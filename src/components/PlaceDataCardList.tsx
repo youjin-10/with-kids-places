fec00609192ab3f6b582ea72cb2da114d8f32676
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2023-10-29 17:42:55 +0900

    update(PlaceDataCardList.tsx): key

--- src/components/PlaceDataCardList.tsx
+++ src/components/PlaceDataCardList.tsx
@@ -9,7 +9,7 @@ const PlaceDataCardList = ({ placeList }: { placeList: PlaceData[] }) => {
       <Text fontWeight="semibold">총 {placeList.length} 곳</Text>
 
       {placeList.map((p, i) => {
-        return <PlaceDataCard place={p} />;
+        return <PlaceDataCard key={i} place={p} />; // note: 리스트 변경이 없으므로 일단 i 사용
       })}
     </Box>
   );
