21e8e564135c67ecbab7135120f02e41477a180b
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2023-10-29 17:25:37 +0900

    add types

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type PlaceData = {
+  type: any;
+  name: string;
+  address: string;
+  longitude: string;
+  latitude: string;
+
+  mainMenu: string;
+  discountMenu: string;
+  discountAmount: string;
+};
