29a9b05ceb406c68f157a6efe10296c0c0fd8fb1
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2023-12-05 22:36:49 +0900

    update: 지도 마커 콘텐츠 width 조정

--- src/components/map/MapMarkerContent.tsx
+++ src/components/map/MapMarkerContent.tsx
@@ -4,7 +4,7 @@ import { Badge, Box, Text } from "@chakra-ui/react";
 
 const MapMarkerContent = ({ place }: { place: PlaceData }) => {
   return (
-    <Box p={2}>
+    <Box p={2} width='40'>
       <Badge>{getPlaceTypeKor(place.type)}</Badge>
 
       <Text fontSize="sm" fontWeight="semibold">
